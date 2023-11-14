00192f43319d74e981fc955444d386018c96b8e7
Leeyanhawrt <leeyan1108@gmail.com>
2023-11-14 16:21:42 -0500

    Take a break on register page

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,9 +1,74 @@
+import Button from "./Button";
+import { ChangeEvent, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { registerUser } from "../store/thunks/authActions";
+
 interface RegisterProps {}
 
+interface UserData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
 const Register: React.FC<RegisterProps> = ({}) => {
+  const [inputs, setInputs] = useState<UserData>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const { firstName, lastName, email, password } = inputs;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+    try {
+      const response;
+    } catch (err) {
+      console.log((err as Error)?.message);
+    }
+  };
+
   return (
     <>
       <h1>Register</h1>
+      <form onSubmit={(_e) => handleSubmit}>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password "
+          value={password}
+          onChange={(e) => handleChange(e)}
+        />
+        <Button>Register</Button>
+      </form>
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/stylesheets/application.css.scss";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./i18n/config.js";
+import store from "./store/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
