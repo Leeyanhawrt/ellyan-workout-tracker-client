691bfddfdbd2379b1fa423bdd2e1850e2d84edda
Leeyanhawrt <leeyan1108@gmail.com>
2023-11-15 23:06:26 -0500

    Work on dashboard component

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,22 @@
 import Button from "./Button";
+import { useState, useEffect } from "react";
 
 interface DashboardProps {
   setAuth: (value: boolean) => void;
 }
 
 const Dashboard: React.FC<DashboardProps> = ({ setAuth }) => {
+  const [name, setName] = useState<string>("");
+
+  const getName = async () => {
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <>
       <h1>Dashboard</h1>
