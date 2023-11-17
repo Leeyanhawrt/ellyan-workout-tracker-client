83dcc03ec183e35025527e2d651b0355c465cc2c
Leeyanhawrt <leeyan1108@gmail.com>
2023-11-17 18:13:49 -0500

    Take dinner break

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,6 @@ import TestimonialList from "../components/TestimonialList";
 import paddling_video from "../assets/videos/ndrc-paddling.mp4";
 import "../assets/stylesheets/pages/_p_home.scss";
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
 
 const HomePage = () => {
   const { t } = useTranslation("", { keyPrefix: "pages.home" });
