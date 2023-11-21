8f940bf6587e3166b2ab2e0d54c521690dee32e7
Leeyanhawrt <leeyan1108@gmail.com>
2023-11-21 01:44:56 -0500

    Remove overflow when modal is open

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,21 @@
 import "../assets/stylesheets/components/_Modal.scss";
 import ReactDOM from "react-dom";
 import { ReactNode } from "react";
+import { useEffect } from "react";
 
 interface ModalProps {
   children?: ReactNode;
 }
 
 const Modal: React.FC<ModalProps> = ({ children }) => {
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
+
   return ReactDOM.createPortal(
     <div className="modal">{children}</div>,
     document.querySelector(".modal-container")!
