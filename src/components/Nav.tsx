fef02d7b2f58383155676eb53fc362b415c0f7fc
Leeyanhawrt <leeyan1108@gmail.com>
2023-11-22 13:56:05 -0500

    Add changing navbar color based off scroll

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,33 @@
 import "../assets/stylesheets/layout/_Nav.scss";
 import Button from "./Button";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import className from "classnames";
 
 interface NavProps {}
 
 const Nav: React.FC<NavProps> = ({}) => {
+  const [scrolled, setScrolled] = useState<number>(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      setScrolled(scrollTop);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const classes = className("nav", {
+    "nav-transparent": scrolled < 200,
+  });
+
   return (
-    <nav id="navbar">
+    <nav id="navbar" className={classes}>
       <Link to="/" className="app-title">
         <img
           src={`${
