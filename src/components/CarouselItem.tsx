5aaa053030db3cb5994e5876dd9b7b2371827bc7
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-06 13:51:11 -0500

    Move Carousel to its own css file

--- src/components/CarouselItem.tsx
+++ src/components/CarouselItem.tsx
@@ -1,4 +1,4 @@
-import "/src/assets/stylesheets/components/_CarouselItem.scss";
+import "/src/assets/stylesheets/components/_Carousel.scss";
 import ExerciseList from "../scenes/dashboard/ExerciseList";
 import DailyWorkout from "../scenes/dashboard/DailyWorkout";
 
