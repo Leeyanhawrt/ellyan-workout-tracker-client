e34b9f35ef1382e05a7df26bff437c3b475533bf
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-06 21:16:27 -0500

    Add replace to login route so users are able to go back to home page" "

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
                 !isAuthenticated ? (
                   <Login setAuth={setAuth} />
                 ) : (
-                  <Navigate to="/dashboard/workout_program" />
+                  <Navigate to="/dashboard/workout_program" replace />
                 )
               }
             />
