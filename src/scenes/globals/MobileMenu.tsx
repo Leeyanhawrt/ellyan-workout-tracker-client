88992c35b5548affb2f9c23a9f1660d0576685ae
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-14 13:01:36 -0800

    Make hackey change for now to display admin routes"

--- src/scenes/globals/MobileMenu.tsx
+++ src/scenes/globals/MobileMenu.tsx
@@ -2,12 +2,16 @@ import "/src/assets/stylesheets/components/_MobileMenu.scss";
 import MobileMenuItem from "./MobileMenuItem";
 import { useAuth, useAuthUpdate } from "../../contexts/AuthContext";
 import { Link } from "react-router-dom";
+import { useUser } from "../../contexts/UserContext";
 
 interface MobileMenuProps {}
 
 const MobileMenu: React.FC<MobileMenuProps> = ({}) => {
+  const user = useUser();
   const authStatus = useAuth();
   const setAuth = useAuthUpdate();
+
+  const isAdmin = user?.roles?.includes("admin");
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -15,11 +19,20 @@ const MobileMenu: React.FC<MobileMenuProps> = ({}) => {
     window.location.reload();
   };
 
-  const menuItems = [
+  let menuItems = [
     { to: `/dashboard/workout_program`, resource: `Workout Program` },
     { to: `/dashboard/edit_record`, resource: `Personal Record` },
     { to: `/dashboard/preferences`, resource: `Profile` },
   ];
+
+  const adminItems = [
+    { to: `/admin/workout_programs`, resource: `Admin Workout Programs` },
+    { to: `/admin/manage_users`, resource: `Admin Manage Users` },
+  ];
+
+  if (isAdmin) {
+    menuItems = menuItems.concat(adminItems);
+  }
 
   return (
     <ul id="mobile-menu">
