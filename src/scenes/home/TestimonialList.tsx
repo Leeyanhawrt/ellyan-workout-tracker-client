355209f9da7181cc59432d0d319084668f19c900
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-13 20:35:34 -0800

    Run build command

--- src/scenes/home/TestimonialList.tsx
+++ src/scenes/home/TestimonialList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import TestimonialItem from "./TestimonialItem";
 import "/src/assets/stylesheets/components/_Testimonial.scss";
 import useAxios from "../../hooks/useAxios";
