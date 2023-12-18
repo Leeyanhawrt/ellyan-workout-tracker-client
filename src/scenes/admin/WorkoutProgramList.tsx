a9dfb409e1a8039b1ec3484561f96c5de83a0e51
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-18 04:34:46 -0500

    Take break to sleep

--- src/scenes/admin/WorkoutProgramList.tsx
+++ src/scenes/admin/WorkoutProgramList.tsx
@@ -1,18 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState, ChangeEvent } from "react";
 import useAxios from "../../hooks/useAxios";
 import WorkoutProgramItem from "./WorkoutProgramItem";
 import { WorkoutProgram } from "../dashboard/WorkoutProgram";
 import "/src/assets/stylesheets/components/_Table.scss";
 import { RiPlayListAddFill } from "react-icons/ri";
+import { postData } from "../../utils/api";
+import { toast } from "react-toastify";
 
 interface WorkoutProgramListProps {}
 
+type WorkoutProgramDetail = {
+  programName: string | undefined;
+};
+
 const WorkoutProgramList: React.FC<WorkoutProgramListProps> = ({}) => {
-  const {
-    data: workoutPrograms,
-    loading,
-    fetchData,
-  } = useAxios<WorkoutProgram[]>(
+  const [showAddProgram, setShowAddProgram] = useState<boolean>(false);
+  const [inputs, setInputs] = useState<WorkoutProgramDetail>({
+    programName: undefined,
+  });
+  const [programList, setProgramList] = useState<WorkoutProgram[]>([]);
+
+  const { programName } = inputs;
+
+  const { data, loading, fetchData } = useAxios<WorkoutProgram[]>(
     [],
     `/admin/workout_programs`,
     `Users List`,
@@ -23,6 +33,41 @@ const WorkoutProgramList: React.FC<WorkoutProgramListProps> = ({}) => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    setProgramList(data);
+  }, [data]);
+
+  const toggleAddProgram = () => {
+    setShowAddProgram((prevShow) => !prevShow);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const appendProgram = (newProgram: WorkoutProgram) => {
+    setProgramList([...programList, newProgram]);
+  };
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const response = await postData(
+      `/admin/workout_programs/`,
+      { ...inputs },
+      true
+    );
+
+    if (response?.status === 201) {
+      appendProgram(response?.data?.dailyWorkout);
+    }
+
+    setShowAddProgram(false);
+    toast.success("Successfully Added New Program");
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -32,9 +77,22 @@ const WorkoutProgramList: React.FC<WorkoutProgramListProps> = ({}) => {
       <thead>
         <tr>
           <th>Program Name</th>
-          <th>
-            <RiPlayListAddFill className="add-program" />
-          </th>
+          {!showAddProgram ? (
+            <th onClick={toggleAddProgram} className="add-program-container">
+              <RiPlayListAddFill className="add-program" />
+            </th>
+          ) : (
+            <th>
+              <input
+                onChange={handleChange}
+                type="text"
+                name="programName"
+                id="programName"
+                placeholder="Program Name"
+                value={programName}
+              />
+            </th>
+          )}
         </tr>
       </thead>
       <tbody>
