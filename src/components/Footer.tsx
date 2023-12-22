51c30b18485f8f6f14ec1ab11607ba6d50ebbce7
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-22 01:41:21 -0500

    Add footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,54 @@
 import "../assets/stylesheets/layout/_Footer.scss";
+import { IoLogoGithub } from "react-icons/io5";
+import { IoLogoLinkedin } from "react-icons/io";
+import { IoLogoInstagram } from "react-icons/io";
+import { useTranslation } from "react-i18next";
 
 interface FooterProps {}
 
 const Footer: React.FC<FooterProps> = ({}) => {
-  return <footer></footer>;
+  const { t } = useTranslation("", { keyPrefix: "layouts.footer" });
+
+  const socialsList = [
+    {
+      logo: <IoLogoLinkedin />,
+      href: "https://www.linkedin.com/in/leeyan-haw/",
+    },
+    {
+      logo: <IoLogoGithub />,
+      href: "https://github.com/Leeyanhawrt",
+    },
+    {
+      logo: <IoLogoInstagram />,
+      href: "https://www.instagram.com/leeyanhawrt/",
+    },
+  ];
+
+  return (
+    <footer>
+      <div className="content-container">
+        <div className="footer-content">
+          <div className="socials-container">
+            <ul className="socials-list">
+              {socialsList.map((social) => {
+                return (
+                  <li>
+                    <a href={social.href} target="_blank">
+                      {social.logo}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="made-with-love">
+            <hr></hr>
+            <p>{t("designedBy")}</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
