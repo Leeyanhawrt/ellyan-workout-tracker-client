1a69b0ea73526a97c156fe0f6bfb337cca83d56b
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-31 19:49:19 -0500

    Implement form handler for microcycle phase

--- src/scenes/dashboard/MicrocycleItem.tsx
+++ src/scenes/dashboard/MicrocycleItem.tsx
@@ -1,6 +1,8 @@
 import "/src/assets/stylesheets/components/_WorkoutProgram.scss";
 import { Microcycle } from "./Microcycle";
+import axios from "axios";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 interface MicrocycleItemProps {
   microcycle: Microcycle;
@@ -21,6 +23,31 @@ const MicrocycleItem: React.FC<MicrocycleItemProps> = ({
     setPhaseInput(e.target.value);
   };
 
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.put(
+        `${import.meta.env.VITE_APP_BACKEND}/admin/microcycle/${microcycle.id}`,
+        {
+          phaseInput,
+        },
+        {
+          headers: { token: localStorage.token },
+        }
+      );
+      console.log(response);
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        const errorMessage = err.response?.data?.error;
+        console.error(errorMessage);
+        toast.error(errorMessage);
+      } else {
+        console.error((err as Error)?.message);
+        toast.error((err as Error)?.message);
+      }
+    }
+  };
+
   return (
     <div onClick={() => onClick(microcycle.id)} className={className}>
       <div className="microcycle-text">
@@ -28,7 +55,7 @@ const MicrocycleItem: React.FC<MicrocycleItemProps> = ({
         <p>{microcycle.phase}</p>
       </div>
       {edittable && (
-        <form className="microcycle-form">
+        <form className="microcycle-form" onSubmit={handleSubmit}>
           <input
             type="text"
             value={phaseInput}
